db580b0e6f91c83f0e3d2516fe51d5b376171938
hariachmad <hari08achmad@gmail.com>
2026-04-28 21:16:03 +0700

    280426

--- main.py
+++ main.py
@@ -1,26 +1,84 @@
 import asyncio
-import time
-from playwright.async_api import async_playwright
+from datetime import datetime, timedelta
+from patchright.async_api import async_playwright
+import random
+import sys
+
+TARGET_HOUR = 21
+TARGET_MINUTE = 15
+TARGET_SECOND = 00
+
+async def wait_until_target():
+    now = datetime.now()
+    target = now.replace(
+        hour=TARGET_HOUR,
+        minute=TARGET_MINUTE,
+        second=TARGET_SECOND,
+        microsecond=0
+    )
+
+    if now >= target:
+        target += timedelta(days=1)
+
+    while True:
+        now = datetime.now()
+        remaining = (target - now).total_seconds()
+
+        if remaining <= 0:
+            break
+
+        hrs = int(remaining // 3600)
+        mins = int((remaining % 3600) // 60)
+        secs = int(remaining % 60)
+
+        sys.stdout.write(f"\rCountdown: {hrs:02d}:{mins:02d}:{secs:02d}")
+        sys.stdout.flush()
+
+        await asyncio.sleep(0.1)
+
+    print("\nWaktu target tercapai!")
+
+
 async def execute():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        page = await browser.new_page()
+        browser = await p.chromium.launch(
+            channel="msedge",
+            headless=False,
+            args=[
+                '--disable-blink-features=AutomationControlled',
+            ]
+        )
         
+        context = await browser.new_context()
+        page = await context.new_page()
+        
+        await page.goto("http://localhost:3000", wait_until="load")
+
+        await wait_until_target()
+
+        print("Mulai hunting tombol...")
+
         while True:
-            await page.goto("https://dyandraglobalstore-02.com/", wait_until="load")
-            print("PAGE LOADED")
-            target = page.locator("button:has-text('10 April 2026')").first
+            await page.goto("http://localhost:3000", wait_until="load")
+
+            target = page.locator("button:has-text('29 April 2026')").first
             
             try:
-                await target.wait_for(timeout=2000)
+                await target.wait_for(timeout=3000)
                 is_disabled = await target.is_disabled()
                 print("Disabled:", is_disabled)
+
                 if not is_disabled:
-                    await target.click(force=True)
+                    await target.click()
                     print("CLICKED!")
                     break
+
             except:
                 print("Button belum ada")
-                await asyncio.sleep(0.05)
-        await asyncio.sleep(9999999999)
+
+            await asyncio.sleep(random.uniform(0.01, 0.03))
+
+        await asyncio.sleep(999999)
+
+
 asyncio.run(execute())
